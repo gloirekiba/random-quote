6787ac35c5f60925ea1c23e4e2b36d2ea594c74f
gloirekiba <gloirekiba21@gmail.com>
2023-01-23 23:00:50 +0100

    🎉 first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Random Quote Machine
+
+***Random Quote Machine is a web app that displays random quotes from famous people.***
+
+## How to run 
+
+1. Clone the repository
+2. Open the index.html file in your browser
+3. Or you can visit the [live demo](https://example.com)
+
+## How to contribute
+
+1. Fork the repository
+2. Clone the repository
+3. Make your changes
+4. Commit and push your changes
+5. Create a pull request
+
+
+## Technologies used
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) - The markup language used
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) - The style sheet language used
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - The programming language used
+
+
+- [GitHub Pages](https://pages.github.com/) - The hosting service used
+  
+- [Git](https://git-scm.com/) - The version control system used
+
+- [GitHub](https://github.com/) - The remote repository hosting service used
+
+- [Visual Studio Code](https://code.visualstudio.com/) - The code editor used
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Acknowledgments
+
+[MDN](https://developer.mozilla.org/en-US/) - The web docs used
+
+- *Hat tip to anyone whose code was used*
+
+## Author
+
+* Gloire KIBA - [gloirekiba](https://github.com/gloirekiba)
+
+## Contributors
+
+* none
+
+
---
+++ app.js
@@ -0,0 +1,57 @@
+const QUOTE_API_URL = "https://api.quotable.io/random";
+const USER_API_URL = "https://randomuser.me/api/";
+
+// Select the DOM elements
+const authorImg = document.getElementById("authorImg");
+const authorName = document.getElementById("authorName");
+const quoteText = document.getElementById("quoteText");
+const loader = document.getElementById("loader");
+const newQuoteBtn = document.getElementById("newQuote");
+const tweetQuoteBtn = document.getElementById("tweetQuote");
+
+// Add event listeners
+newQuoteBtn.addEventListener("click", refreshQuote);
+tweetQuoteBtn.addEventListener("click", tweetQuote);
+
+async function refreshQuote() {
+  // Show the loader
+  loader.classList.remove("hidden");
+
+  // Fetch the quote and user details from the API
+  const [quote, user] = await Promise.all([
+    fetchApi(QUOTE_API_URL),
+    fetchApi(USER_API_URL),
+  ]).catch((err) => {
+    window.alert("Something went wrong! \n" + err);
+    console.log(err);
+    return;
+  });
+
+  // This is a hack to show the loader for 2 seconds even if the API is fast enough to respond in less than 2 seconds
+  setTimeout(() => {
+    // Hide the loader
+    loader.classList.add("hidden");
+  }, 2000);
+
+  // Set the quote and author details
+  authorImg.src = user.results[0].picture.large;
+  authorName.innerText = quote.author;
+  quoteText.innerText = quote.content;
+}
+
+// Call the function once to load the quote on page load
+refreshQuote();
+
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorName.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+async function fetchApi(url) {
+  return await fetch(url)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => Promise.reject(err));
+}
